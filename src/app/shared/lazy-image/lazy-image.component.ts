9da6a96f0d62eccb510fe1e9ef48776ee8865f1f
Noa <noasalgadodev@gmail.com>
2023-11-04 19:57:46 +0100

    feat: copy gif link to clipboard

--- src/app/shared/lazy-image/lazy-image.component.ts
+++ src/app/shared/lazy-image/lazy-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'lazy-image',
@@ -6,15 +6,31 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./lazy-image.component.css'],
 })
 export class LazyImageComponent {
+  @ViewChild('link') copyLink!: ElementRef;
+
   @Input()
   public url!: string;
 
   @Input()
   public title!: string;
 
+  @Input()
+  public gifLink!: string;
+
   public hasLoaded = false;
 
   onLoad() {
     this.hasLoaded = true;
+  }
+
+  public copyToClipboard() {
+    navigator.clipboard.writeText(this.gifLink);
+    this.copyLink.nativeElement.style.backgroundColor = '#598EF3';
+    this.copyLink.nativeElement.textContent = 'Link copied!';
+
+    setTimeout(() => {
+      this.copyLink.nativeElement.style.backgroundColor = '#1e293b';
+      this.copyLink.nativeElement.innerHTML = `<span class="material-symbols-outlined"> link </span> Copy GIF Link`;
+    }, 2000);
   }
 }
